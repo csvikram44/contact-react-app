fb5a5ced3dd5152a9ed8bc4450e6070e7e743602
Vikram Kumar <cs.vikram44@gmail.com>
2021-09-16 18:59:14 +0530

    Design changes added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <div className="ui menu">
-            <div className="ui container center">
-                <h2>Contact Manager</h2>
+            
+                <div className="title">
+                    <h2>Contact Manager</h2>
+                </div>
+                <div className="menu-items">
                 <Link to="/add">
                     <button className="ui button blue right floated">Add Contact</button>
                 </Link>
-            </div>
+                </div>
+  
         </div>
     );
 };
